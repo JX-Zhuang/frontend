2aa2707d82b9f526c48f1cc61460e1079c21c083
JX-Zhuang <who.zhuang@gmail.com>
2023-04-21 13:00:12 +0800

    update

--- react-test/src/App.js
+++ react-test/src/App.js
@@ -1,23 +1,23 @@
-// import React, { useEffect, useState, Suspense } from 'react';
-// import CardList from './components/CardList';
-// import UserList from './components/UserList';
-// import UserListSWR from './components/UserListSWR';
-// import useList from './hooks/useList';
-// import Test from './RxTest/components';
-// function App() {
-//   const [show, setShow] = useState(true);
-//   return (
-//     <div className="App">
-//       <Test />
-//       {/* <UserList /> */}
-//       {/* {show && <UserList />} */}
-//       {/* <button onClick={() => setShow(!show)}>click</button> */}
-//       {/* <CardList /> */}
-//       {/* <UserListSWR /> */}
-//       {/* <UserListSWR /> */}
-//     </div>
-//   );
-// }
+import React, { useEffect, useState, Suspense } from 'react';
+import CardList from './components/CardList';
+import UserList from './components/UserList';
+import UserListSWR from './components/UserListSWR';
+import useList from './hooks/useList';
+import Test from './RxTest/components';
+function App() {
+  const [show, setShow] = useState(true);
+  return (
+    <div className="App">
+      <Test />
+      {/* <UserList /> */}
+      {/* {show && <UserList />} */}
+      {/* <button onClick={() => setShow(!show)}>click</button> */}
+      {/* <CardList /> */}
+      {/* <UserListSWR /> */}
+      {/* <UserListSWR /> */}
+    </div>
+  );
+}
 
-// export default App;
+export default App;
 
--- react-test/src/index.js
+++ react-test/src/index.js
@@ -1,36 +1,65 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import App from './myReact';
-import reportWebVitals from './reportWebVitals';
-
-
-const store = [];
-let index = 0;
-const useState = (initial) => {
-  const currentIndex = index;
-  if (store[index] === undefined) {
-    store[index] = [initial, (state) => {
-      store[currentIndex][0] = state;
-      index = 0;
-      root.render(<App />);
-    }];
-  }
-  return [store[index][0], store[index++][1]];
-};
-const App = () => {
-  const [i, setI] = useState(0);
-  const [j, setJ] = useState(1);
-  useEffect(() => {
-    setI(j + 1);
-  }, [j]);
-  return <div>
-    <button onClick={() => setI(i + 1)}>i:{i}</button>
-    <br />
-    <button onClick={() => setJ(j + 1)}>j:{j}</button>
-  </div>
-}
-
-
+import App from './myReact';
+// let stateIndex = 0;
+// const createUseState = () => {
+//   const store = [];
+//   const createSetter = (index) => {
+//     return function (newState) {
+//       store[index][0] = newState;
+//       render();
+//     }
+//   }
+//   // let index = 0;
+//   const useState = (initial) => {
+//     if (store[stateIndex] === undefined) {
+//       store[stateIndex] = [initial, createSetter(stateIndex)];
+//     }
+//     return [store[stateIndex][0], store[stateIndex++][1]];
+//   };
+//   return useState;
+// }
+// const useState = createUseState();
+// let effectIndex = 0;
+// const createUseEffect = () => {
+//   const store = [];
+//   const useEffect = (dispatch, deps) => {
+//     let update = false;
+//     let prevDeps = store[effectIndex];
+//     if (prevDeps === undefined) {
+//       update = true;
+//     } else {
+//       for (const i in prevDeps) {
+//         if (deps[i] !== prevDeps[i]) {
+//           update = true;
+//           break;
+//         }
+//       }
+//     }
+//     if (update) {
+//       dispatch();
+//     }
+//     store[effectIndex++] = deps;
+//   };
+//   return useEffect;
+// };
+// const useEffect = createUseEffect();
+// const App = () => {
+//   const [i, setI] = useState(0);
+//   const [j, setJ] = useState(1);
+//   useEffect(() => {
+//     console.log('mount');
+//   }, []);
+//   useEffect(() => {
+//     console.log('effect')
+//     setI(j + 1);
+//   }, [j]);
+//   return <div>
+//     <button onClick={() => setI(i + 1)}>i:{i}</button>
+//     <br />
+//     <button onClick={() => setJ(j + 1)}>j:{j}</button>
+//   </div>
+// }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -38,8 +67,3 @@ root.render(
   <App />
   // </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
